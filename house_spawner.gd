75bcad94e536ad8a7de88f46f4ca24b1278b2c98
Mathias Johannsen <mathias.j@arkulpa.at>
2024-03-05 21:26:10 +0100

    spawn ground plane

--- house_spawner.gd
+++ house_spawner.gd
@@ -1,6 +1,7 @@
 extends Node
 
 var house = preload("res://house.tscn")
+var ground = preload("res://ground.tscn")
 
 var earthCircumference = 40075000;
 
@@ -27,17 +28,25 @@ func latLonToCoordsInMeters(lat, lon):
 		lat * earthCircumference / 360
 	)
 
-func setCameraPosition(coords: Vector2):
+func setCameraPosition():
+	var coords: Vector2 = latLonToCoordsInMeters(houses[0].x, houses[0].y)
 	get_viewport().get_camera_3d().position.x = coords.x
 	get_viewport().get_camera_3d().position.y = 100
 	get_viewport().get_camera_3d().position.z = coords.y+150
+
+func spawnGround():
+	var coords: Vector2 = latLonToCoordsInMeters(houses[0].x, houses[0].y)
+	var inst: StaticBody3D = ground.instantiate()
+	inst.transform.origin = Vector3(coords.x,0,coords.y)
+	add_child(inst)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	fetchCoordinates()
 	for house in houses:
 		spawnHouse(latLonToCoordsInMeters(house.x, house.y))
-	setCameraPosition(latLonToCoordsInMeters(47.4050971, 9.7426105))
+	setCameraPosition()
+	spawnGround()
 
 #
 ## Called every frame. 'delta' is the elapsed time since the previous frame.
