8930f5e583be14064a49520b308ea3e3c5afbb78
Mathias Johannsen <mathias.j@arkulpa.at>
2024-03-05 21:30:44 +0100

    refactor

--- house_spawner.gd
+++ house_spawner.gd
@@ -8,19 +8,13 @@ var earthCircumference = 40075000;
 var houses: Array[Vector2] = []
 
 func fetchCoordinates():
-	houses.append(Vector2(47.4050971, 9.7426105))
-	houses.append(Vector2(47.4050583, 9.7436344))
-	houses.append(Vector2(47.4054232, 9.7430115))
-	houses.append(Vector2(47.4054460, 9.7426622))
-	houses.append(Vector2(47.4047326, 9.7424377))
-	houses.append(Vector2(47.4052588, 9.7428630))
-	houses.append(Vector2(47.4053796, 9.7431154))
-
-
-func spawnHouse(coords: Vector2):
-	var inst: StaticBody3D = house.instantiate()
-	inst.transform.origin = Vector3(coords.x,0,coords.y)
-	add_child(inst)
+	houses.append(latLonToCoordsInMeters(47.4050971, 9.7426105))
+	houses.append(latLonToCoordsInMeters(47.4050583, 9.7436344))
+	houses.append(latLonToCoordsInMeters(47.4054232, 9.7430115))
+	houses.append(latLonToCoordsInMeters(47.4054460, 9.7426622))
+	houses.append(latLonToCoordsInMeters(47.4047326, 9.7424377))
+	houses.append(latLonToCoordsInMeters(47.4052588, 9.7428630))
+	houses.append(latLonToCoordsInMeters(47.4053796, 9.7431154))
 
 func latLonToCoordsInMeters(lat, lon):
 	return Vector2(
@@ -28,14 +22,19 @@ func latLonToCoordsInMeters(lat, lon):
 		lat * earthCircumference / 360
 	)
 
+func spawnHouse(coords: Vector2):
+	var inst: StaticBody3D = house.instantiate()
+	inst.transform.origin = Vector3(coords.x,0,coords.y)
+	add_child(inst)
+
 func setCameraPosition():
-	var coords: Vector2 = latLonToCoordsInMeters(houses[0].x, houses[0].y)
+	var coords: Vector2 = houses[0]
 	get_viewport().get_camera_3d().position.x = coords.x
 	get_viewport().get_camera_3d().position.y = 100
 	get_viewport().get_camera_3d().position.z = coords.y+150
 
 func spawnGround():
-	var coords: Vector2 = latLonToCoordsInMeters(houses[0].x, houses[0].y)
+	var coords: Vector2 = houses[0]
 	var inst: StaticBody3D = ground.instantiate()
 	inst.transform.origin = Vector3(coords.x,0,coords.y)
 	add_child(inst)
@@ -44,7 +43,7 @@ func spawnGround():
 func _ready():
 	fetchCoordinates()
 	for house in houses:
-		spawnHouse(latLonToCoordsInMeters(house.x, house.y))
+		spawnHouse(house)
 	setCameraPosition()
 	spawnGround()
 
