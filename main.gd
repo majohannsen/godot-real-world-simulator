c5cd962d4c034ddc037cac1ea1d7960514762cae
Mathias Johannsen <mathias.j@arkulpa.at>
2024-04-14 18:27:28 +0200

    add debug addon, use web mercator projection

--- main.gd
+++ main.gd
@@ -4,8 +4,10 @@ const earthCircumference = 40075000
 const chunk_size = 1000
 const lat_span = chunk_size / 100000.0
 const lon_span = chunk_size / 100000.0
-const lat_center = 47.41903911
-const lon_center = 9.726977348
+#const lat_center = 47.41903911
+#const lon_center = 9.726977348
+const lat_center = 67.831433
+const lon_center = 20.277365
 
 var center = Vector2(latToMeter(lat_center), lonToMeter(lat_center, lon_center))
 
@@ -16,16 +18,24 @@ var ground = preload("res://ground.tscn")
 var loadedChunks: Dictionary = {}
 
 func latLonToCoordsInMeters(lat, lon):
+	print(lat, " ", lon)
+	print(log(tan(PI/4 + lat/2)))
+	print(Vector2(
+		latToMeter(lat),
+		lonToMeter(lat, lon)
+	))
 	return Vector2(
 		latToMeter(lat),
 		lonToMeter(lat, lon)
 	) - center
 
+## use Web Mercator projection
+
 func latToMeter(lat):
-	return lat * earthCircumference / 360
+	return -floor((1/(2*PI))*pow(2, 17.5)*(PI-(log(tan(PI/4 + lat/2)))))
 
 func lonToMeter(lat, lon):
-	return lon * earthCircumference / 360 * cos(deg_to_rad(lat))
+	return floor((1/(2*PI))*pow(2, 17.5)*(PI+lon))
 
 func setCameraPosition():
 	$Player.position.x = 0
@@ -49,6 +59,7 @@ func getCurrentChunk():
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	setCameraPosition()
+	DebugDraw3D.scoped_config().set_thickness(1)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -56,3 +67,16 @@ func _process(delta):
 		loadedChunks[getCurrentChunk()] = true
 		spawnChunk(getCurrentChunk())
 		print(loadedChunks)
+	for key in loadedChunks.keys():
+		var chunk = Vector2(key)
+		var lat1 = lat_center - lat_span/2 + lat_span * chunk.x
+		var lat2 = lat_center + lat_span/2 + lat_span * chunk.x
+		var lon1 = lon_center - lon_span/2 + lon_span * chunk.y
+		var lon2 = lon_center + lon_span/2 + lon_span * chunk.y
+		var pos1 = latLonToCoordsInMeters(lat1, lon1)
+		var pos2 = latLonToCoordsInMeters(lat2, lon2)
+		DebugDraw3D.draw_line(Vector3(pos1.x, 0, pos1.y), Vector3(pos2.x, 0, pos2.y), Color(1, 1, 0))
+		DebugDraw3D.draw_line(Vector3(pos1.x, 0, pos1.y), Vector3(pos1.x, 0, pos2.y), Color(1, 1, 0))
+		DebugDraw3D.draw_line(Vector3(pos1.x, 0, pos1.y), Vector3(pos2.x, 0, pos1.y), Color(1, 1, 0))
+		DebugDraw3D.draw_line(Vector3(pos2.x, 0, pos1.y), Vector3(pos2.x, 0, pos2.y), Color(1, 1, 0))
+		DebugDraw3D.draw_line(Vector3(pos1.x, 0, pos2.y), Vector3(pos2.x, 0, pos2.y), Color(1, 1, 0))
